89a399533695772a4e6132f68388dfa32abe44e1
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-05-16 10:14:54 +0200

    Add Comments and CommentList skeleton

--- app/posts/[id]/loading.tsx
+++ app/posts/[id]/loading.tsx
@@ -1,12 +1,10 @@
-import { SkeletonPostArticle } from '@/ui/skeletons';
+import { SkeletonComments, SkeletonPostArticle } from '@/ui/skeletons';
 
 export default function Loading() {
   return (
     <>
-      <div className="mb-6 s:mb-10">
-        <SkeletonPostArticle />
-      </div>
-      <div>Loading comments...</div>
+      <SkeletonPostArticle />
+      <SkeletonComments />
     </>
   );
 }
