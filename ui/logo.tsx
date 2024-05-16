5ba1af3873a1b945a68115198027f2175510326e
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-05-16 11:49:03 +0200

    Add content to Logo component

--- ui/logo.tsx
+++ ui/logo.tsx
@@ -1,5 +1,15 @@
+import { BookOpenIcon } from 'lucide-react';
+import Link from 'next/link';
+
 interface LogoProps {}
 
 export default function Logo({}: LogoProps) {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <Link href="/" className="flex items-center gap-2">
+        <BookOpenIcon size={20} />
+        <span className="text-sm font-medium">JSONPlaceholder Blog</span>
+      </Link>
+    </div>
+  );
 }
